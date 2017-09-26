30e8db3db5f8b24488c14ba345c9a62b252b388c
TOM <tom710236@gmail.com>
2017-09-26 15:06:24 +0800

    我的最愛功能

--- app/src/main/java/com/example/tom/bankapp/MainActivity.java
+++ app/src/main/java/com/example/tom/bankapp/MainActivity.java
@@ -12,21 +12,60 @@ import android.os.Vibrator;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.ckfree.common.SimpleSearchBar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     String token = "null";
     SQLiteDatabase db;
+    GridView mGridView;
+    ArrayList<String> itemName;
+    ArrayList<Integer> imageRes;
+    SimpleAdapter adapter;
+    ArrayList<LinkedHashMap<String, String>> myList;
+    LinkedHashMap<String, String> map;
+    // 九宮格全部功能的圖片
+    private int[] imageRes2 = {R.drawable.pig64, R.drawable.ic_search_black_24dp,
+            R.drawable.pig64, R.drawable.ic_search_black_24dp, R.drawable.ic_search_black_24dp, R.drawable.ic_search_black_24dp,
+            R.drawable.pig64, R.drawable.pig64, R.drawable.pig64};
+    //九宮格全部功能
+    private String[] itemName2 = {"审前调查", "需求评估", "在册人员", "请销假", "集中教育",
+            "个别教育", "心理测评", "生活量表", "矫正方案"};
+    private HashSet<Integer> mCheckSet = new HashSet<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //啟動service 讓7.0的手機可以常駐後台
-        Intent it = new Intent(MainActivity.this,Delay.class);
-        startService(it);
 
+        //預設九宮格的設定
+        setmArrayList();
+        //search 的設定
+        setmSearch();
+        //啟動service 讓7.0的手機可以常駐後台
+        Intent it = new Intent(MainActivity.this, Delay.class);
+        startService(it);
+        //取出token
         getToken();
-        Log.e("TOKEN",Application.FCMToken);
+        Log.e("TOKEN", Application.FCMToken);
         //跳出dialog
+        //S為推播的內容 (FirebaseMessageingService 那邊調整onMessageReceived)
         MyNotice.getInstance().setOnMessageReceivedListener(new MyNotice.OnMessageReceivedListener() {
             @Override
             public void onMessageReceived(final String s) {
@@ -38,6 +77,21 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+        //九宮格的設定
+        setmGridView();
+        //新增我的最愛
+        setmListView();
+
+
+    }
+
+    //定义九宮格点击事件监听器
+    public class GridViewItemOnClick implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getApplicationContext(), position + "", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void mDialog(String message) {
@@ -54,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
         alertDlg.show();
 
     }
+
     //假設收到雲端通知時APP是在裝置的背景，此時不會在onMessageReceived中收到通知，Android系統會自動將收到的通知顯示在裝置上方，下拉可顯示更多資訊
     //目前好像沒用
     @Override
@@ -62,15 +117,18 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         final Bundle bundle = intent.getExtras();
+        /*
         String msg = bundle.getString("msg");
         if (msg!=null){
             Log.e("FCM", "msg:"+msg);
         }
+        */
 
     }
-    private String getToken(){
-        MyDBhelper MyDB = new MyDBhelper(this,"tblTable",null,1);
-        db=MyDB.getWritableDatabase();
+
+    private String getToken() {
+        MyDBhelper MyDB = new MyDBhelper(this, "tblTable", null, 1);
+        db = MyDB.getWritableDatabase();
         //Cursor c=db2.rawQuery("SELECT * FROM "+"tblTable2", null);   //查詢全部欄位
         Cursor c = db.query("tblTable",                          // 資料表名字
                 null,                                              // 要取出的欄位資料
@@ -80,12 +138,13 @@ public class MainActivity extends AppCompatActivity {
                 null,                                              // Having字串法
                 null);                                             // Order By字串語法(排序)
         //往下一個 收尋
-        while(c.moveToNext()) {
+        while (c.moveToNext()) {
             token = c.getString(c.getColumnIndex("cToken"));
         }
         Application.FCMToken = token;
         return token;
     }
+
     private void unLock() {
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         //获取电源管理器对象
@@ -108,6 +167,153 @@ public class MainActivity extends AppCompatActivity {
         //收到推播會震動
         Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vb.vibrate(1500);
+
+    }
+
+    public void onAdd(View v) {
+        LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+        LinearLayout linear2 = (LinearLayout) findViewById(R.id.linear2);
+        ScrollView linear3 = (ScrollView) findViewById(R.id.linear3);
+        ScrollView linear4 = (ScrollView) findViewById(R.id.linear4);
+        linear.setVisibility(View.GONE);
+        linear2.setVisibility(View.GONE);
+        linear3.setVisibility(View.GONE);
+        linear4.setVisibility(View.VISIBLE);
+    }
+
+    public void onBack(View v) {
+        LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+        LinearLayout linear2 = (LinearLayout) findViewById(R.id.linear2);
+        ScrollView linear3 = (ScrollView) findViewById(R.id.linear3);
+        ScrollView linear4 = (ScrollView) findViewById(R.id.linear4);
+        linear.setVisibility(View.VISIBLE);
+        linear2.setVisibility(View.VISIBLE);
+        linear3.setVisibility(View.VISIBLE);
+        linear4.setVisibility(View.GONE);
+        setmGridView();
+        adapter.notifyDataSetChanged();
+
+    }
+
+    public class SpecialAdapter extends SimpleAdapter {
+
+        public SpecialAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+        }
+
+        @Override
+        public View getView(final int position, View convertView, final ViewGroup parent) {
+            //获取相应的view中的checkbox对象
+            convertView = null;
+            if (convertView == null) {
+                convertView = View.inflate(MainActivity.this, R.layout.item2, null);
+                final CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
+                checkBox.setChecked(mCheckSet.contains(position));
+                checkBox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (((CheckBox) v).isChecked()) {
+                            itemName.add(itemName2[position]);
+                            imageRes.add(imageRes2[position]);
+                            mCheckSet.add(position);
+                        } else {
+
+                            for(int i = 0 ; i<itemName.size() ; i++){
+                                if(itemName.get(i).equals(itemName2[position])){
+                                    Log.e("打勾", itemName.get(i));
+                                    itemName.remove(i);
+                                    i--;
+                                    mCheckSet.remove(position);
+                                }
+                            }
+                            for (int i= 0 ; i<imageRes.size() ; i++){
+                                if(imageRes.get(i).equals(itemName2[position])){
+                                    imageRes.remove(i);
+                                    i--;
+                                }
+                            }
+
+                        }
+
+                    }
+                });
+
+            }
+            return super.getView(position, convertView, parent);
+        }
+
+    }
+
+    private void setmGridView() {
+        //九宮格的設定
+        mGridView = (GridView) findViewById(R.id.MyGridView);
+        List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+        int length = itemName.size();
+        for (int i = 0; i < length; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImageView", imageRes.get(i));
+            map.put("ItemTextView", itemName.get(i));
+            data.add(map);
+        }
+        //为itme.xml添加适配器
+        SimpleAdapter simpleAdapter = new SimpleAdapter(MainActivity.this,
+                data, R.layout.item, new String[]{"ItemImageView", "ItemTextView"}, new int[]{R.id.ItemImageView, R.id.ItemTextView});
+        mGridView.setAdapter(simpleAdapter);
+        //为mGridView添加点击事件监听器
+        mGridView.setOnItemClickListener(new GridViewItemOnClick());
+    }
+    private void setmSearch(){
+        SimpleSearchBar mysearchbar = (SimpleSearchBar) findViewById(R.id.mysearchbar);
+        View displayview = findViewById(R.id.imageView7);//跟随searchbar显示隐藏的view
+        mysearchbar.init(displayview, new SimpleSearchBar.SearchBarWathcer() {
+            @Override
+            protected void _onTextChanged(String s) {
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setmListView(){
+        myList = new ArrayList();
+        for (int i = 0; i < itemName2.length; i++) {
+            map = new LinkedHashMap<String, String>();
+            map.put("1", itemName2[i]);
+            myList.add(map);
+        }
+        ListView listView = (ListView) findViewById(R.id.listView);
+        adapter = new SpecialAdapter(
+                MainActivity.this,
+                myList,
+                R.layout.item2,
+                new String[]{"1", "checkbox"},
+                new int[]{R.id.textView2});
+        listView.setAdapter(adapter);
+
+    }
+    private void setmArrayList(){
+        //預設九宮格的功能
+        itemName = new ArrayList<>();
+        itemName.add("审前调查");
+        itemName.add("需求评估");
+        mCheckSet.add(0);
+        mCheckSet.add(1);
+
+        //預設九宮格圖片
+        imageRes = new ArrayList<>();
+        imageRes.add(R.drawable.pig64);
+        imageRes.add(R.drawable.ic_search_black_24dp);
+    }
+    private void setmCheckboxFirst(){
+        final CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+        for(int i = 0 ; i<itemName.size();i++){
+            if(myList.get(i).get("1").equals(itemName.get(i))){
+                Log.e("相同",myList.get(i).get("1"));
+                Log.e("mcheck", String.valueOf(mCheckSet));
+
+
+            }
+        }
 
     }
 }
